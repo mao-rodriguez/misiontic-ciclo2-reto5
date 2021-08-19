15aee4caa22dc2cad54f2c1b31e0a2ee168feee7
marr4631 <mauricio.rodriguez@christus.co>
2021-08-19 15:59:21 -0500

    Se adiciona interfaz grafica para consultar tres reportes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,36 @@
 import view.VistaRequerimientosReto4;
+import java.awt.EventQueue;
 
 public class App {
     public static void main( String[] args ){        
 
+        EventQueue.invokeLater(new Runnable(){
+
+            @Override
+            public void run() {
+                try {
+                    VistaRequerimientosReto4 frame = new VistaRequerimientosReto4();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                
+            }
+            
+        });
+
         //Casos de prueba 
 
         //Requerimiento 1 - Reto3
-        VistaRequerimientosReto4.requerimiento1();
-        System.out.println();
+        // VistaRequerimientosReto4.requerimiento1();
+        // System.out.println();
 
-         //Requerimiento 2 - Reto3
-         VistaRequerimientosReto4.requerimiento2();
-         System.out.println();
+        //  //Requerimiento 2 - Reto3
+        //  VistaRequerimientosReto4.requerimiento2();
+        //  System.out.println();
 
-        //Requerimiento 5 - Reto3
-        VistaRequerimientosReto4.requerimiento3();
-        System.out.println();
+        // //Requerimiento 5 - Reto3
+        // VistaRequerimientosReto4.requerimiento3();
+        // System.out.println();
     }
 }
--- src/main/java/view/VistaRequerimientosReto4.java
+++ src/main/java/view/VistaRequerimientosReto4.java
@@ -10,76 +10,153 @@ import model.vo.Requerimiento3;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class VistaRequerimientosReto4 {
-    
-    public static final ControladorRequerimientosReto4 controlador = new ControladorRequerimientosReto4();
-    
-    public static void requerimiento1(){
-        System.out.println("*** Lideres por Salario ***");
-        
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.*;
+//import javax.swing.AbstractButton;
 
+public class VistaRequerimientosReto4 extends JFrame {
+
+
+    public static final ControladorRequerimientosReto4 controlador = new ControladorRequerimientosReto4();
+    private JPanel contentPane;
+    private JTextArea txtArea;
+
+    public VistaRequerimientosReto4(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(550, 300, 900, 700);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5,5,5,5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JLabel lblTitulo = new JLabel("Misión TIC 2022 Ciclo 2 Reto 5");
+        lblTitulo.setBounds(28, 6, 61, 6);
+        contentPane.add(lblTitulo);
+
+        JLabel lblautor = new JLabel("Mauricio Rodríguez Rivera");
+        lblTitulo.setBounds(28, 34, 208, 16);
+        contentPane.add(lblautor);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(28, 70, 737, 455);
+        contentPane.add(scrollPane);
+
+        txtArea = new JTextArea();
+        scrollPane.setViewportView(txtArea);
+
+        // Botón Para requerimiento1
+        JButton btnRequerimiento1 = new JButton("Requerimiento 1");
+        btnRequerimiento1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                requerimiento1();
+            }
+        });
+            btnRequerimiento1.setBounds(28, 537, 150, 31);
+            contentPane.add(btnRequerimiento1);
+
+        // Botón Para requerimiento1
+        JButton btnRequerimiento2 = new JButton("Requerimiento 2");
+        btnRequerimiento2.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                requerimiento2();
+            }
+        });
+        btnRequerimiento2.setBounds(200, 537, 150, 31);
+            contentPane.add(btnRequerimiento2);
+
+        // Botón Para requerimiento3
+        JButton btnRequerimiento3 = new JButton("Requerimiento 3");
+        btnRequerimiento3.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                requerimiento3();
+            }
+        });
+        btnRequerimiento3.setBounds(372, 537, 150, 31);
+            contentPane.add(btnRequerimiento3);
+        
+        // Botón para limpiar ventana
+        JButton btnLimpiar = new JButton("Limpiar");
+        btnLimpiar.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                txtArea.setText("");
+            }
+        });
+        btnLimpiar.setBounds(550, 537, 150, 31);
+            contentPane.add(btnLimpiar);
+    }
+    
+    private void requerimiento1(){
         try{
             ArrayList<Requerimiento1> listaConsulta = controlador.consultarRequerimiento_1();
-
+            String tabla = "\t\t*** Lideres por Salario ***\n\n\tID\tNOMBRE\tAPELLIDO\tSALARIO\n\n\n";
             for (Requerimiento1 requerimiento1 : listaConsulta) {
-                System.out.printf("El lider %s %s con Id %d Tiene un salario de %d",
-                requerimiento1.getNombre(),
-                requerimiento1.getPrimer_apellido(),
-                requerimiento1.getId_lider(),
-                (int)requerimiento1.getSalario()
-                );
-                System.out.println();
-            }
-            
-
-        }catch(SQLException e){
+                tabla += "\t";
+                tabla += requerimiento1.getNombre();
+                tabla += "\t";
+                tabla += requerimiento1.getPrimer_apellido();
+                tabla += "\t";
+                tabla += requerimiento1.getId_lider();
+                tabla += "\t";
+                tabla += (int)requerimiento1.getSalario();
+                tabla += "\n";
+            };
+            txtArea.setText(tabla);
+        }
+        catch(SQLException e){
             System.err.println("Ha ocurrido un error!"+e.getMessage());
         }
     }
 
-    public static void requerimiento2(){     
-
-        System.out.println("*** Proyectos por Tipo ***");
-
+    public void requerimiento2(){
         try{
             ArrayList<Requerimiento2> listaConsulta = controlador.consultarRequerimiento_2();
-
+            String tabla = "\t\t*** Proyectos por Tipo ***\n\n\tID\tCONSTRUCTORA\tCIUDAD\tESTRATO\n\n\n";
             for (Requerimiento2 requerimiento2 : listaConsulta) {
-                System.out.printf("El proyecto de ID_Proyecto %d de la constructora: %s de la ciudad %s, tiene un estrato asignado de %s",
-                requerimiento2.getId_proyecto(),
-                requerimiento2.getConstructora(),
-                requerimiento2.getCiudad(),
-                requerimiento2.getEstrato()
-                );
-                System.out.println();
-            }
-            
-
-        }catch(SQLException e){
+                tabla += "\t";
+                tabla += requerimiento2.getId_proyecto();
+                tabla += "\t";
+                tabla += requerimiento2.getConstructora();
+                tabla += "\t";
+                tabla += requerimiento2.getCiudad();
+                tabla += "\t";
+                tabla += requerimiento2.getEstrato();
+                tabla += "\n";
+            };
+                txtArea.setText(tabla);
+        }
+        catch(SQLException e){
             System.err.println("Ha ocurrido un error!"+e.getMessage());
         }
     }
 
-    public static void requerimiento3(){
-
-        System.out.println("*** Lideres por Nombre ***");
-
+    public void requerimiento3(){
         try{
             ArrayList<Requerimiento3> listaConsulta = controlador.consultarRequerimiento_3();
+            String tabla = "\t\t*** Lideres por Nombre ***\n\n\tID\tNOMBRE\tAPELLIDO\n\n\n";
 
             for (Requerimiento3 requerimiento3 : listaConsulta) {
-                System.out.printf("El Lider con Id %d se llama %s %s",
-                requerimiento3.getId_lider(),
-                requerimiento3.getNombre(),
-                requerimiento3.getPrimer_apellido()
-                );
-                System.out.println();
+                tabla += "\t";
+                tabla += requerimiento3.getId_lider();
+                tabla += "\t";
+                tabla += requerimiento3.getNombre();
+                tabla += "\t";
+                tabla += requerimiento3.getPrimer_apellido();
+                tabla += "\n";
             }
-            
+                txtArea.setText(tabla);
 
-        }catch(SQLException e){
+        } catch(SQLException e){
             System.err.println("Ha ocurrido un error!"+e.getMessage());
         }
     }
-
 }
